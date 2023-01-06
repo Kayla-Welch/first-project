890e550d7a057c5105f28abb9236e6ba3ea0cef1
Kayla-Welch <122100249+Kayla-Welch@users.noreply.github.com>
2023-01-06 08:37:05 -0500

    expanded the 'DateTime' class and added the 'People' class

--- constants.py
+++ constants.py
@@ -1,5 +1,21 @@
 class DateTime:
-    CURRENT_YEAR = 2023
+    # sample year, day, and month
+    CURRENT_YEAR = 2000
     CURRENT_DAY = 1
-    CURRENT_MONTH = 1
-    
+    CURRENT_MONTH = 
+    # maximum hour is 24
+    CURRENT_HOUR = 7
+    CURRENT_TIME = 30
+
+class People:
+    JohnSmith {
+        "name": "John Smith",
+        "age": 40,
+        "car": "Toyota"
+    }
+
+    JaneSmith {
+        "name": "Jane Smith",
+        "age": 35,
+        "car": "Ford"
+    }
